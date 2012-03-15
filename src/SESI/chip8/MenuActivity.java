4bd69abd65323b2571e419745055c5d611dff431
Alexandre JOANNOU <alexandre.joannou@gmail.com>
2012-03-15 22:19:31 +0100

    ajout des valeurs au spinner de sélection du screen dans MenuActivity (action sur le layout et le string .xml également)

--- src/SESI/chip8/MenuActivity.java
+++ src/SESI/chip8/MenuActivity.java
@@ -5,22 +5,37 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.app.Activity;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
-public class MenuActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.menuactivity);
 
-            Button next = (Button) findViewById(R.id.button1);
-            next.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View view) {
-                    Intent myIntent = new Intent(view.getContext(), GameActivity.class);
-                    //Intent myIntent = new Intent(view.getContext(), SESIChip8.class);
-                    startActivityForResult(myIntent, 0);
-                }
+public class MenuActivity extends Activity
+{
+		public void onCreate(Bundle savedInstanceState)
+		{
+				super.onCreate(savedInstanceState);
+				setContentView(R.layout.menuactivity);
 
-            });
-        }
+
+				// spinner screen
+				Spinner screenSpinner = (Spinner) findViewById(R.id.screenSpinner);
+				ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+								this, R.array.screenType, android.R.layout.simple_spinner_item);
+				adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+				screenSpinner.setAdapter(adapter);
+
+
+
+
+				Button go = (Button) findViewById(R.id.buttonGo);
+				go.setOnClickListener(new View.OnClickListener()
+								{
+								public void onClick(View view) {
+								Intent myIntent = new Intent(view.getContext(), GameActivity.class);
+								//Intent myIntent = new Intent(view.getContext(), SESIChip8.class);
+								startActivityForResult(myIntent, 0);
+								}
+
+								});
+		}
 }
